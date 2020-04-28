0304e7a4e9649162dddd7935687f4feec2e604b5
Andrea De Martino <ademartino1992@gmail.com>
2020-04-28 15:26:37 +0200

    Create: Bonus Task

--- main.js
+++ main.js
@@ -4,36 +4,50 @@ $(document).ready(function() {
 // Servendoci di handlebars stampiamo tutto a schermo.
 // In questo momento non è importante la parte grafica.
 
-$.ajax({
-	url: 'https://flynn.boolean.careers/exercises/api/array/music',
-	method: 'GET',
-	success: function(data){
-		var self = data.response;
-		for (var i = 0; i < self.length; i++){
+var select = $('#selection');
+var cdContainer = $('.cds-container');
 
-			var source = $('#cd-template').html();
-			var template = Handlebars.compile(source);
-			var cdContainer = $('.cds-container');
+select.change(function(){
+	var genre = $(this).val();
+	cdContainer.children().remove();
 
-			var cd = ({
-				poster: self[i].poster,
-				title: self[i].title,
-				author: self[i].author,
-				year: self[i].year
-			})
-
-			var html = template(cd);
-			cdContainer.append(html);
+	$.ajax({
+		url: 'https://flynn.boolean.careers/exercises/api/array/music',
+		method: 'GET',
+		success: function(data){
+			var self = data.response;
+			for (var i = 0; i < self.length; i++){
+				if (self[i].genre == genre){
+					var source = $('#cd-template').html();
+					var template = Handlebars.compile(source);
+					var cdContainer = $('.cds-container');
+		
+					var cd = ({
+						poster: self[i].poster,
+						title: self[i].title,
+						author: self[i].author,
+						year: self[i].year
+					})
+		
+					var html = template(cd);
+					cdContainer.append(html).hide().fadeIn(200);
+				}
+				
+			}
+		},
+		error: function(){
+			console.log('Api in errore');
 		}
-	},
-	error: function(){
-		console.log('Api in errore');
-	}
+	})
 })
 
+
+	
+
+
+
 // Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
-// Endpoint API
-// https://flynn.boolean.careers/exercises/api/array/music
+
 
 
 
